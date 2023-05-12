105b14657e063388706cad423ed6fb1fb393f377
Hiemstra <harald.hiemstra@capgemini.com>
2023-05-12 23:02:34 +0200

    openlayers basic setup

---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+  map: Map | undefined;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+
+  }
+
+
+
+}
